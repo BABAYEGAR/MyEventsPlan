02dcb47478c59534cc35bd301047ddac6378d75e
Haruna Salim <salxsaa@gmail.com>
2017-01-21 02:46:29 +0100

    Added Service helper classes...

---
+++ MyEventPlan.Data.Service/Configuration/Config.cs
@@ -0,0 +1,10 @@
+﻿namespace MyEventPlan.Data.Service.Configuration
+{
+    public static class Config
+    {
+        public static string SupportEmailAddress => "bhuinfo@support.com";
+        //public static string SupportDisplayName => ConfigurationManager.AppSettings["SupportEmailDisplayName"];
+
+        //public static string SupportEmailAddress => ConfigurationManager.AppSettings["SupportEmail"];
+    }
+}
---
+++ MyEventPlan.Data.Service/EmailService/MailerDaemon.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.IO;
+using System.Net.Mail;
+using System.Text;
+using System.Web;
+using Event.Data.Objects.Entities;
+using MyEventPlan.Data.Service.Configuration;
+
+namespace MyEventPlan.Data.Service.EmailService
+{
+    public class MailerDaemon
+    {
+        /// <summary>
+        ///     This method sends an email containing a username and password to a newly created user
+        /// </summary>
+        /// <param name="user"></param>
+        public void NewUser(AppUser user)
+        {
+            var message = new MailMessage
+            {
+                From = new MailAddress(Config.SupportEmailAddress),
+                Subject = "New User Details",
+                Priority = MailPriority.High,
+                SubjectEncoding = Encoding.UTF8,
+                Body = GetEmailBody_NewUserCreated(user),
+                IsBodyHtml = true
+            };
+            //message.To.Add(Config.DevEmailAddress);
+            message.To.Add(user.Email);
+            try
+            {
+                new SmtpClient().Send(message);
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+
+        /// <summary>
+        ///     Html page content for the new user email
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        private static string GetEmailBody_NewUserCreated(AppUser user)
+        {
+            return
+                new StreamReader(HttpContext.Current.Server.MapPath("~/EmailTemplates/NewUserCreated.html")).ReadToEnd()
+                    .Replace("DISPLAYNAME", user.Firstname)
+                    .Replace("USERNAME", user.Email)
+                    .Replace("PASSWORD", user.Password)
+                    .Replace("URL", "http://10.10.15.77/bhuinfo/Account/Login")
+                    .Replace("ROLE", user.Firstname)
+                    .Replace("FROM", Config.SupportEmailAddress);
+        }
+        /// <summary>
+        ///     This method sends an email containing a username and password to a newly created user
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="loggedinuser"></param>
+
+
+        public void ResetUserPassword(AppUser user)
+        {
+            var message = new MailMessage();
+
+            message.From = new MailAddress(Config.SupportEmailAddress);
+            message.To.Add(user.Email);
+            message.Subject = "New Password";
+            message.Priority = MailPriority.High;
+            message.SubjectEncoding = Encoding.UTF8;
+            message.Body = GetEmailBody_NewPasswordCreated(user);
+            message.IsBodyHtml = true;
+            try
+            {
+                new SmtpClient().Send(message);
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+        /// <summary>
+        /// This contains the html and passed values for the password reset emails
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        private static string GetEmailBody_NewPasswordCreated(AppUser user)
+        {
+            return
+                new StreamReader(HttpContext.Current.Server.MapPath("~/EmailTemplates/ResetPassword.html"))
+                    .ReadToEnd()
+                    .Replace("FROM", Config.SupportEmailAddress)
+                    .Replace("DISPLAYNAME", user.Firstname)
+                    .Replace("URL", "http://10.10.15.77/bhuinfo/Account/ResetPassword/" + user.AppUserId);
+        }
+        /// <summary>
+        /// This method sends emails to the support of the bhuinfo application
+        /// </summary>
+        /// <param name="senderName"></param>
+        /// <param name="senderMessage"></param>
+        /// <param name="email"></param>
+        public void ContactUs(string senderName,string senderMessage,string email)
+        {
+            var message = new MailMessage();
+            message.From = new MailAddress(email);
+            message.To.Add(Config.SupportEmailAddress);
+            message.Subject = "New Contact";
+            message.Priority = MailPriority.High;
+            message.SubjectEncoding = Encoding.UTF8;
+            message.Body = GetEmailBody_ContactUs(senderName, senderMessage);
+            message.IsBodyHtml = true;
+            try
+            {
+                new SmtpClient().Send(message);
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+        /// <summary>
+        /// This method contains the html and passed values for the contact us email
+        /// </summary>
+        /// <param name="senderName"></param>
+        /// <param name="senderMessage"></param>
+        /// <returns></returns>
+        private static string GetEmailBody_ContactUs(string senderName, string senderMessage)
+        {
+            return
+                new StreamReader(HttpContext.Current.Server.MapPath("~/EmailTemplates/ContactUs.html"))
+                    .ReadToEnd()
+                    .Replace("DISPLAYNAME", senderName)
+                    .Replace("URL", "http://10.10.15.77/bhuinfo")
+                    .Replace("MESSAGE", senderMessage);
+        }
+    }
+}
